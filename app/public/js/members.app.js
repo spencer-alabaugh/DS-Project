691b8940620562970f1bcce93d06b16d208f79f2
aanchal <aanwadh@iu.edu>
2019-10-15 22:23:17 -0400

    Vue JS changes
    Changes for members table in DataEntry.html to pick data from javascript file

---
+++ app/public/js/members.app.js
@@ -0,0 +1,33 @@
+var mambersApp = new Vue({
+  el: '#membersApp',
+  data: {
+    members:  [{
+        "firstName":"Aanchal",
+        "lastName":"Wadhwani",
+        "radioNumber":"R1",
+        "stationNumber":"S1",
+        "active":"yes"
+      },
+      {
+        "firstName":"Saharsh",
+        "lastName":"Arora",
+        "radioNumber":"R2",
+        "stationNumber":"S2",
+        "active":"no"
+      },
+      {
+        "firstName":"Luis",
+        "lastName":"Cervantes",
+        "radioNumber":"R3",
+        "stationNumber":"S3",
+        "active":"yes"
+      },
+      {
+        "firstName":"Spencer",
+        "lastName":"Alabaugh",
+        "radioNumber":"R4",
+        "stationNumber":"S4",
+        "active":"no"
+      }]
+  }
+})
