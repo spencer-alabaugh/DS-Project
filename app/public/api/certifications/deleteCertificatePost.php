5dea4e072f6e4c281fd5ac583deca707ef60f039
Luis C <ldcervan@iu.edu>
2019-10-22 19:52:28 -0400

    delete is working on certifications, edit still isn't working

---
+++ app/public/api/certifications/deleteCertificatePost.php
@@ -0,0 +1,32 @@
+<?php
+
+// 0. Validate my data
+
+// Step 1: Get a datase connection from our help class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  'DELETE FROM Certification
+  WHERE certificationId = ?;'
+);
+
+$stmt->execute([
+/*  $_POST['firstName'],
+  $_POST['lastName'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
+  $_POST['isActive'],
+  $_POST['address'],
+  $_POST['email'],
+  $_POST['gender'],
+  $_POST['position'],*/
+  $_POST['certificationId']
+
+]);
+
+// TODO: Error checking?!
+
+// Step 4: Output
+header('HTTP/1.1 303 See Other');
+header('Location: ../certifications/');
---
+++ app/public/js/edit_certificate.app.js
@@ -0,0 +1,63 @@
+var edit_certificateApp = new Vue({
+  el: '#edit_certificateApp',
+  data: {
+    certificate: {}
+  },
+  methods: {
+    handleSubmit() {
+      fetch('api/certifications/editCertificatePost.php', {
+        method:'POST',
+        body: JSON.stringify(this.certificate),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      // .then( response => response.json() )
+      // .then( json => {edit_membersApp.employee.push( json[0] ) })
+      // .catch( err => {
+      //   console.error('MEMBER POST ERROR:');
+      //   console.error(err);
+  // });
+   this.handleReset();
+  },
+
+  handleDelete() {
+    fetch('api/certifications/deleteCertificatePost.php', {
+      method:'POST',
+      body: JSON.stringify(this.certificate),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+ this.handleReset();
+},
+
+
+
+
+
+
+  handleReset() {
+    this.certificate = {
+      certificationId: '',
+      certificationName: '',
+      experationPeriod: '',
+      certifyingAgency: ''
+    }
+  },
+  handleRowClick(certifications){
+    edit_certificateApp.certificate = certifications;
+  }
+},
+  created() {
+  }
+})
+
+/* function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}*/
