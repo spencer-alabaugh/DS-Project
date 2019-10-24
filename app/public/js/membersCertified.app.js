2471f2da29a8dce40d8a97896a47bf94c6d06d1e
Saharsh Arora <saharsharora1@gmail.com>
2019-10-23 22:59:20 -0400

    Revert "added filter to details table"
    This reverts commit 3a2491772a42b293679530966937338574d08836.

--- app/public/js/membersCertified.app.js
+++ app/public/js/membersCertified.app.js
@@ -14,22 +14,3 @@ var membersCertifiedApp = new Vue({
     this.fetchMemberCertifications();
   }
 });
-var expiredCertificationApp = new Vue({
-  el: '#certificationsMemberApp',
-  data: {
-    certifications: [],
-    filter: {
-      certName: ''
-    }
-  },
-  methods: {
-    fetchCertifications() {
-      fetch('api/certifications/expired_certifications.php')
-      .then(response => response.json())
-      .then(json => { expiredCertificationApp.certifications = json })
-    }
-  },
-  created() {
-    this.fetchCertifications();
-  }
-});
